e6907232f57978618f3487fed254a52904723b2b
dwyerd28 <dwyerd@lafayette.edu>
2025-12-03 17:46:47 -0500

    Add files via upload

--- hw_pins.h
+++ hw_pins.h
@@ -1,11 +1,11 @@
 #pragma once
 // Substitutes for PIN_X_STEP & PIN_X_DIR
-#define PIN_MOTOR_A_STEP 10
-#define PIN_MOTOR_A_DIR  11
+#define PIN_MOTOR_A_STEP 2
+#define PIN_MOTOR_A_DIR  3
 
 //  Substitutes for PIN_Y_STEP & PIN_Y_DIR
-#define PIN_MOTOR_B_STEP 12
-#define PIN_MOTOR_B_DIR  13
+#define PIN_MOTOR_B_STEP 6
+#define PIN_MOTOR_B_DIR  7
 
 // servo
 #define PIN_SERVO   15
