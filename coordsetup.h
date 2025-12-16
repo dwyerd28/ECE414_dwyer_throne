849eda1099b76cc8ed51ba8c98b6bfbd81acff35
dwyerd28 <dwyerd@lafayette.edu>
2025-12-15 22:31:35 -0500

    Update coordsetup.h

--- coordsetup.h
+++ coordsetup.h
@@ -1,17 +1,17 @@
-// coordsetup.h
 #pragma once
 #include <stddef.h>
 #include <stdbool.h>
 
-// A single point in the drawing path
+//structure for coordsetup.c
 typedef struct {
     float x_mm;    
     float y_mm;      
     bool  pen_down;  
 } DrawPoint;
 
-// Number of points in the path
+// number of points in a path
 size_t coord_get_count(void);
 
-// Get pointer to point index (0..count-1)
+
 const DrawPoint *coord_get_point(size_t index);
+
