65a5142a64f233128b71c14c51fabd607a3f6b95
dwyerd28 <dwyerd@lafayette.edu>
2025-12-15 22:30:41 -0500

    Update coordsetup.c

--- coordsetup.c
+++ coordsetup.c
@@ -1,21 +1,19 @@
-// coordsetup.c
 #include "coordsetup.h"
 
 
 
 static const DrawPoint g_path[] = {
-    // Travel move from (0,0) to (20,20)
-    {  0.0f,  0.0f, false },  // start (assumed current pos)
-    { 20.0f, 20.0f, false },  // move with pen up
-
-    // Now draw a square 40x40 mm
-    { 20.0f, 20.0f, true  },  // pen down, start of square
+    
+    {  0.0f,  0.0f, false },  
+    { 20.0f, 20.0f, false },  
+//square
+    { 20.0f, 20.0f, true  },  
     { 60.0f, 20.0f, true  },
     { 60.0f, 60.0f, true  },
     { 20.0f, 60.0f, true  },
-    { 20.0f, 20.0f, true  },  // back to start
+    { 20.0f, 20.0f, true  },  
 
-    // Lift pen and move back home
+
     {  0.0f,  0.0f, false },
     
 
@@ -31,4 +29,5 @@ const DrawPoint *coord_get_point(size_t index) {
     if (index >= coord_get_count()) return NULL;
     return &g_path[index];
 }
+
 
