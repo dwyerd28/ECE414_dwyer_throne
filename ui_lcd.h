c426746e4e647e3e0aed8268ce1cac185d30c6a6
dwyerd28 <dwyerd@lafayette.edu>
2025-12-15 22:55:54 -0500

    Update ui_lcd.h

--- ui_lcd.h
+++ ui_lcd.h
@@ -2,11 +2,12 @@
 #pragma once
 #include <stddef.h>
 
-// Initialize TFT LCD and draw static elements
+// initialize TFT LCD and draw static elements
 void ui_lcd_init(void);
 
-// Update percent text & progress bar based on completed points
+// update percent text & progress bar based on completed points
 void ui_lcd_update_progress(size_t done, size_t total);
 
-// Show a "Done" message when drawing complete
+//shows done once completed
 void ui_lcd_show_done(void);
+
