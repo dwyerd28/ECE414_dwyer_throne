21874abcbdc1c616e5a9deb72e414bb87e9bf1a6
dwyerd28 <dwyerd@lafayette.edu>
2025-12-15 22:32:24 -0500

    Update limits.h

--- limits.h
+++ limits.h
@@ -1,10 +1,10 @@
-// limits.h
 #pragma once
 #include <stdbool.h>
 
-// Initialize limits (if needed)
+// Initialize limits 
 void limits_init(void);
 
-// Returns true if (x_mm, y_mm) is inside allowed area
+// returns true if (x_mm, y_mm) is inside allowed area
 bool limits_within(float x_mm, float y_mm);
+
 
